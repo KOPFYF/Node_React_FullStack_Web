3a52eae1b91a4041ff7f661ad4a3eaadc02d39ac
KOPFYF <fei8@purdue.edu>
2021-07-08 20:22:03 -0400

    ignore

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express'); // import package
+const app = express();
+
+// route handler
+
+// app: express app to register this route handler with
+// get: get request, watch for incoming http requests (get, post, put, delete, patch)
+// '/': watch for incoming '/', like subfolder, http://localhost:5000 => http://localhost:5000/
+// req: object of incoming request
+// res: object of outgoing response
+app.get('/', (req, res) => {
+    res.send({hi: 'there'}); // send some json back to whoever made this request
+});
+
+// by https://dashboard.heroku.com/apps or local test default=5000
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT); // port id
