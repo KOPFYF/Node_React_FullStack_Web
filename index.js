37d0a115a40449b72018d5aea56f4ed702c37013
KOPFYF <fei8@purdue.edu>
2021-07-09 14:05:28 -0400

    google-oauth

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-const express = require('express'); // import package
+const express = require('express'); // import express as express
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-// route handler
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'] // ouath credential works here
+  })
+);
+
+
+app.get('/auth/google/callback',
+  passport.authenticate('google') // redirect to server
+);
+
+
+// by https://dashboard.heroku.com/apps or local test default=5000
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT); // port id
+
+
+
+
+
+
+
+// code practice 0: hello world of route handler
 // app: express app to register this route handler with
 // get: get request, watch for incoming http requests (get, post, put, delete, patch)
 // '/': watch for incoming '/', like subfolder, http://localhost:5000 => http://localhost:5000/
 // req: object of incoming request
 // res: object of outgoing response
-app.get('/', (req, res) => {
-    res.send({bye: '888'}); // send some json back to whoever made this request
-});
-
-// by https://dashboard.heroku.com/apps or local test default=5000
-const PORT = process.env.PORT || 5000; 
-app.listen(PORT); // port id
+// app.get('/', (req, res) => {
+//     res.send({bye: '888'}); // send some json back to whoever made this request
+// });
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-This project includes the fullstack web development codes using node.js, React, Redux, Express server, PassportJS and MangoDB.
+In this project I designed a RESTful web application to provide customers with mass survey delivery as well as advanced reports on received feedbacks. The application contains multiple functionalities, including user authentication, payment handling, email delivery, and report generation.
+
+Tech Stack:
+
+Frontend is implemented in React, Redux, JavaScript, HTML5 and CSS.
+
+Backend is implemented in Node.js and Express.js
+
+Database is Mongodb 4.4.6.
+
+Involved third-party APIs: sendGrid API, Stripe API, Google OAuth authentication, Heroku API.
